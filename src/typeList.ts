b5352c7c8feea046530f6e0554df88c490c5fb34
hiro <hmaeyama0510@gmail.com>
2021-05-30 04:32:20 +0900

    type don't have to memorize everything..

---
+++ src/typeList.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/** TypeScript Basics */
+
+//boolean
+let bool: boolean = true;
+
+// number
+let num: number = 0;
+
+// string
+let str: string = "Hello";
+
+// Array
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+//tuple
+let tuple: [number, string] = [0, "A"];
+
+//any
+let any;
+
+//void
